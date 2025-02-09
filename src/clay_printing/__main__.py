36edd71cd2d32d03cb822400a4a260e531d72206
WeiTing1991 <weitingchen0502@gmail.com>
2025-02-09 19:21:39 +0100

    feat: add basic ads hanndler

---
+++ src/clay_printing/__main__.py
@@ -0,0 +1,33 @@
+from .hal.plc import PLC
+from datetime import datetime
+
+# =================================================================================
+"""Global value"""
+
+#TODO: change it
+CLIENT_ID = "5.57.158.168.1.1"  # PLC AMSNETID
+CLIENT_IP = "192.168.30.11"
+NOW_DATE = datetime.now().date().strftime("%Y%m%d")  # Date
+
+# =================================================================================
+
+process_running = input("Do you want to start the process? (y/n): ")
+process_stop = input("Do you want to stop the process? (y/n): ")
+
+def main():
+  """Main function to run the process."""
+
+  plc = PLC(netid=CLIENT_ID, ip=CLIENT_IP)
+  plc.connect()
+
+  while process_running:
+
+    if not process_running or process_stop:
+      print("The process is not running.")
+      plc.close()
+      break
+
+
+if __name__ == '__main__':
+    main()
+    pass
---
+++ src/clay_printing/hal/plc.py
@@ -0,0 +1,62 @@
+import pyads
+from pydantic import BaseModel, Field
+from typing import List, Any
+from threading import Lock
+
+class PLC(BaseModel):
+    """Hardware abstraction class for reading and writing variables from and to PLC."""
+
+    netid: str = Field(..., description="Net ID of the PLC")
+    ip: str = Field(..., description="IP address of the PLC")
+
+    connection: Any = Field(default=None, description="PLC connection object")
+    lock_dict: Lock = Field(default_factory=Lock, description="Lock for dictionary operations")
+    lock_ads: Lock = Field(default_factory=Lock, description="Lock for ADS communication")
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.connection = pyads.Connection(self.netid, pyads.PORT_TC3PLC1)
+
+    def close(self):
+        """Close the connection to the PLC."""
+        if self.connection and self.connection.is_open:
+            self.connection.close()
+        print("PLC connection closed")
+
+    def connect(self) -> bool:
+        """Connect to the PLC."""
+        with self.lock_ads:
+            if not self.connection.is_open:
+                self.connection.open()
+            try:
+                self.connection.read_device_info()
+            except pyads.ADSError as e:
+                print(f"Error: {e}")
+                return False
+            else:
+                print(f"Connection: {self.connection.is_open}")
+                return True
+    def read_variables(self):
+        """Reads all structs from PLC and stores inside class."""
+        if not self.connect():
+            raise AdsConnectionError(
+                "Could not read variable from PLC, PLC connection failed."
+            )
+        with self.lock_ads:
+            raise NotImplementedError
+
+    def write_variables(self):
+        """Writes all variables that have been set."""
+        if not self.connect():
+            raise AdsConnectionError(
+                "Could not read variable from PLC, PLC connection failed."
+            )
+        with self.lock_ads:
+            raise NotImplementedError
+
+    class Config:
+        arbitrary_types_allowed = True  # Allow non-Pydantic types (e.g., pyads.Connection, Lock)
+
+
+class AdsConnectionError(Exception):
+    pass
